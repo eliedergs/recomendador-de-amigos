dff2f508b257facbf340ea6809e0b1c47f867f89
Elieder Gomes <elieder.silveira@consorciomagalu.com.br>
2022-08-31 20:11:48 -0300

    feat: adicionar storage e repositório base

---
+++ src/infrastructure/repositories/base.repository.ts
@@ -0,0 +1,40 @@
+import Storage from '../storage';
+
+export abstract class BaseRepository<T = {}> {
+  private _key = '';
+
+  constructor(key) {
+    this._key = key;
+  }
+
+  add(data: T) {
+    let _data = Storage.get<T[]>(this._key) || [];
+    _data = [..._data, data];
+
+    Storage.set<T[]>(this._key, _data);
+  }
+
+  find(itemKey?: keyof T, value?: any) {
+    const _data = Storage.get<T[]>(this._key) || [];
+
+    return itemKey ? _data.filter((item) => item[itemKey] === value) : _data;
+  }
+
+  findOne(itemKey: keyof T, value: any) {
+    const _data = Storage.get<T[]>(this._key) || [];
+
+    return _data.find((item) => item[itemKey] === value);
+  }
+
+  delete(itemKey: keyof T, value: any) {
+    let _data = Storage.get<T[]>(this._key) || [];
+
+    _data = _data.filter((item) => item[itemKey] !== value);
+
+    Storage.set<T[]>(this._key, _data);
+  }
+
+  clear() {
+    Storage.set<T[]>(this._key, []);
+  }
+}
---
+++ src/infrastructure/storage/index.ts
@@ -0,0 +1,21 @@
+import NodeCache from 'node-cache';
+
+export default class Storage {
+  private static _cache: NodeCache;
+
+  static initializeCache() {
+    if (!Storage._cache) Storage._cache = new NodeCache();
+  }
+
+  static set<T>(key: string, data: T) {
+    this._cache.set(key, data);
+  }
+
+  static get<T>(key: string) {
+    return this._cache.get<T>(key);
+  }
+
+  static clear(key: string) {
+    return this._cache.set(key, null);
+  }
+}
