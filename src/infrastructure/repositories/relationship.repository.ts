91210d9b7828b8a28ad8c0bb8d396e9263314ea2
Elieder Gomes <elieder.silveira@consorciomagalu.com.br>
2022-09-01 16:04:42 -0300

    refactor: remover arquivos não utilizados

--- src/infrastructure/repositories/relationship.repository.ts
+++
@@ -1,3 +0,0 @@
-import { BaseRepository } from './base.repository';
-
-export class RelationshipRepository extends BaseRepository {}
--- src/infrastructure/utils/Regex.ts
+++
@@ -1,4 +0,0 @@
-export const maskCnpj = /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g;
-export const maskPhone = /^(\d{2})(\d?)(\d{4})(\d{4})/g;
-export const maskCep = /^(\d{2})(\d{3})(\d{3})/g;
-export const maskNoNumbers = /[^0-9]/g;
