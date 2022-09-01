2f1fc2ae4af537f2425524c3d79ff5ba00ff9f3c
Elieder Gomes <elieder.silveira@consorciomagalu.com.br>
2022-09-01 09:55:35 -0300

    test: adicionar testes de "relationship-service"

---
+++ __tests__/unit/services/relationship-service.test.ts
@@ -0,0 +1,43 @@
+import { IPerson } from '@/domain/interfaces/person.interface';
+import { IRelationship } from '@/domain/interfaces/relationship.interface';
+import RelationshipService from '@/domain/services/relationship.service';
+import { BaseRepository } from '@/infrastructure/repositories/base.repository';
+import Storage from '@/infrastructure/storage';
+import { describe } from '@jest/globals';
+import { PersonMock } from '../../_fixtures/mocks/person.mock';
+
+describe('Relationship service', () => {
+  const personRepository = new BaseRepository<IPerson>('person');
+
+  beforeAll(() => {
+    Storage.initializeCache();
+  });
+
+  describe('addRelationship', () => {
+    let people: IPerson[];
+
+    beforeAll(() => {
+      people = Array.from({ length: 2 }).map(() =>
+        PersonMock.createPersonDto()
+      );
+
+      Storage.set<IPerson[]>('person', people);
+    });
+
+    it('Should create a relationship between two existing people', () => {
+      const dto = {
+        cpf1: people[0].cpf,
+        cpf2: people[1].cpf,
+      };
+
+      RelationshipService.addRelationship(dto);
+
+      const _relationships = Storage.get<IRelationship[]>('relationship');
+      expect(_relationships).toMatchObject([dto]);
+    });
+  });
+
+  afterEach(() => {
+    personRepository.clear();
+  });
+});
