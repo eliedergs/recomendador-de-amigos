9de2f7fef7e684ce1f366f7e6da7ca1b74c468a1
Elieder Gomes <elieder.silveira@consorciomagalu.com.br>
2022-09-01 16:07:40 -0300

    feat: adicionar "middleware' de validação

--- src/infrastructure/middlewares/ValidationMiddleware.ts
+++ src/infrastructure/middlewares/ValidationMiddleware.ts
@@ -3,34 +3,34 @@ import { NextFunction, Request, Response } from 'express';
 import { validate, ValidationError } from 'class-validator';
 
 import {
-    ClassValidationOptions,
-    ValidationErrorHandler,
-} from '@/domain/interfaces/Utils/Validations';
-import { extractMessages } from '@/infrastructure/utils/Validations';
+  ClassValidationOptions,
+  ValidationErrorHandler,
+} from '@/domain/interfaces/validations.interface';
+import { extractMessages } from '@/infrastructure/utils/validations';
 
 export const validatePayload = (
-    validateAs: any,
-    from: 'body' | 'params' | 'query',
-    options: ClassValidationOptions = {}
+  validateAs: any,
+  from: 'body' | 'params' | 'query',
+  options: ClassValidationOptions = {}
 ) => {
-    const { errorHandler = sendErrors, ...validatorOptions } = options;
+  const { errorHandler = sendErrors, ...validatorOptions } = options;
 
-    return async (req: Request, res: Response, next: NextFunction) => {
-        const toValidate = Object.assign(new validateAs(), req[from]);
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const toValidate = Object.assign(new validateAs(), req[from]);
 
-        const errors = await validate(toValidate, validatorOptions);
+    const errors = await validate(toValidate, validatorOptions);
 
-        if (!!errors?.length) {
-            return errorHandler(res, errors);
-        }
+    if (!!errors?.length) {
+      return errorHandler(res, errors);
+    }
 
-        next();
-    };
+    next();
+  };
 };
 
 export const sendErrors: ValidationErrorHandler = (
-    res: Response,
-    errors: ValidationError[]
+  res: Response,
+  errors: ValidationError[]
 ) => {
-    res.status(400).json(extractMessages(errors));
+  res.status(400).json(extractMessages(errors));
 };
